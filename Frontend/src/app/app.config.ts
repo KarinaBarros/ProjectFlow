8533d6fa46c720a082492d74ddc69ce096782749
Karina Barros <barros_kari@hotmail.com>
2025-04-10 11:14:47 -0300

    Estilização de login e validação de formulários

--- Frontend/src/app/app.config.ts
+++ Frontend/src/app/app.config.ts
@@ -6,10 +6,17 @@ import { authInterceptor } from './interceptors/auth.interceptor';
 import { FormsModule } from '@angular/forms';
 import { importProvidersFrom } from '@angular/core';
 
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
+import { LOCALE_ID } from '@angular/core';
+
+registerLocaleData(localePt);
+
 export const appConfig: ApplicationConfig = {
   providers: [
     importProvidersFrom(FormsModule),
     provideHttpClient(withInterceptors([authInterceptor])),
-    provideRouter(routes)
+    provideRouter(routes),
+    { provide: LOCALE_ID, useValue: 'pt-BR' }
   ]
 };
